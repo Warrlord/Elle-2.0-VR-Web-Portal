605e870003cc1bac6fb8dc70cc2d69e29631aad3
Warrlord <behler.mark1@gmail.com>
2018-09-28 03:40:41 -0400

    Updated Dependencies

--- elle_2.0_web/src/index.js
+++ elle_2.0_web/src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Router, Route, hashHistory } from 'react-router'
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Route path="/" component={App}/>
+    <Route path="*" component={Whoops404}/>
+  </Router>
+  <App />, document.getElementById('root')
+
+);
 registerServiceWorker();
