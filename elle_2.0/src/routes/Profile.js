ce299ff3d91478577cb94f7df9273f075f146907
Warrlord <behler.mark1@gmail.com>
2018-10-04 03:48:36 -0400

    Basis For several Pages

--- elle_2.0/src/routes/Profile.js
+++ elle_2.0/src/routes/Profile.js
@@ -1,1 +1,21 @@
-import React from 'react'
+import FaShield from 'react-icons/lib/fa/shield'
+import { Component } from 'react'
+
+class Profile extends Component {
+
+render() {
+
+	const { name, thumbnail, email, admin, makeAdmin } = this.props
+    return (
+        <div className="member">
+        	<h1>{name} {(admin) ? <FaShield /> : null}</h1>
+        	<a onClick={makeAdmin}>Make Admin</a>
+        	<img src={thumbnail} alt="profile picture" />
+        	<p><a href={`mailto:${email}`}>{email}</a></p>
+
+        </div>
+    )
+}
+}
+
+export default Profile
